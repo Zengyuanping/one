5e1021fbcbb498739ca5b793f1dcc33a1b8b4946
ZYP <1097449451@qq.com>
2020-03-06 10:33:35 +0800

    '组件化开发'

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -1,34 +1,14 @@
-// pages/home/home.js
+// pages/about/about.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    counter: 0
 
   },
-  handleIncrement(event){
-    console.log('-----',event);
-    this.setData({
-      counter:this.data.counter + 1
-    })
-  },
-  handleTabClick(event){
+  handleTabClick(event) {
     console.log(event)
-  },
-  handleIncrementCpn(){
-    // console.log('-----')
-    //最终目的：修改my-sel中的counter
-    //1.获取组件对象
-    const my_sel = this.selectComponent('#sel-id')
-    // console.log(my_sel)
-    // 2.通过setData修改组件中的数据
-    // my_sel.setData({
-    //   counter:my_sel.data.counter + 20
-    // })
-    // 3.通过方法对数据进行修改
-    my_sel.incrementCounter(10)
   },
 
   /**
