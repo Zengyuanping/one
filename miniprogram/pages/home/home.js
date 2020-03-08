a1ad6897f728720c0312fd831308c47d2481a481
ZYP <1097449451@qq.com>
2020-03-08 08:42:59 +0800

    '弹窗和分享'

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,6 +7,60 @@ Page({
   data: {
 
   },
+  handleShowToast(){
+    wx.showToast({
+      title:'Lisa榕',
+      duration:3000,
+      icon:"loading",
+      mask:true,
+      success: function(){
+        console.log('展示弹窗成功')
+      },
+      fail:function(){
+        console.log('展示弹窗失败')
+      },
+      complete:function(){
+        console.log("完成函数调用")
+      }
+    })
+  },
+  handleShowModal(){
+    wx.showModal({
+      title: 'Lisa榕',
+      content: '你好骚啊',
+      // showCancel:false,
+      // cancelText:"再见",
+      cancelColor:"red",
+      success:function(res){
+        console.log(res)
+        if(res.cancel){
+          console.log('jacky平点击了取消按钮')
+        }
+        if(res.confirm){
+          console.log('jacky平点击了确定按钮')
+        }
+      }
+    })
+  },
+  handleShowLoading(){
+    wx.showLoading({
+      title: 'Aily榕',
+      mask: true
+    })
+    setTimeout(()=>{
+      //必须手动
+      wx.hideLoading()
+    },2000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['艾莉','品如',"高珊珊"],
+      itemColor:'red',
+      success:function(res){
+        console.log(res)
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -60,7 +114,12 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (options) {
+    return{
+      title: '你好啊，Lisa榕',
+      path:'/pages/about/about',
+      imageUrl:''
+    }
 
   }
 })
